eed322e399b8bf7bb18959bb741173cdf4d19daa
jstier <stier.jochen@web.de>
2024-03-26 13:54:20 +0100

    initial setup

---
+++ OhsomeNowStatsMock/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StatusBannerComponent } from './status-banner/status-banner.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StatusBannerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ OhsomeNowStatsMock/src/app/status-banner/status-banner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-banner',
+  templateUrl: './status-banner.component.html',
+  styleUrl: './status-banner.component.css'
+})
+export class StatusBannerComponent {
+}

b3eb2b769ee774699b0894228fb6894b5cccf48a
Johannes Link <business@johanneslink.net>
2024-04-18 15:05:01 +0200

    Started work on plain karma config for tdd tests

--- src/tdd/adder.spec.ts
+++ src/tdd/adder.spec.ts
@@ -6,4 +6,9 @@ describe('Adding Numbers', () => {
     const sum = add(0, 0);
     expect(sum).toEqual(0);
   });
+
+  it(`1 + 1 = 2`, () => {
+    const sum = add(1, 1);
+    expect(sum).toEqual(2);
+  });
 });

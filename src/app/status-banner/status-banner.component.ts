cfd24ea782052935760390317f4981d43abe2590
Berend <bcavantilburg@gmail.com>
2024-04-23 14:12:06 +0200

    Status message from Statuscake is displayed.

--- src/app/status-banner/status-banner.component.ts
+++ src/app/status-banner/status-banner.component.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-status-banner',
-  templateUrl: './status-banner.component.html',
-  styleUrl: './status-banner.component.css'
+    selector: 'app-status-banner',
+    templateUrl: './status-banner.component.html',
+    styleUrl: './status-banner.component.css'
 })
-export class StatusBannerComponent {
+export class StatusBannerComponent implements OnInit {
+    announcement: string = ""
+    show: boolean = false;
+
+    hide(): void {
+        this.show = false;
+
+    }
+
+    search(): void {
+
+        const url = 'https://int-stats.now.ohsome.org/statuspage';
+
+        fetch(url).then(res => res.json()).then(data => {
+            if (data["Announce"]) {
+                this.announcement = data["Announce"]
+
+
+                if (this.announcement != "") {
+
+                    this.show = true
+
+                }
+
+
+            } else {
+                console.log("NO announcement in Statuscake available!!!")
+            }
+        });
+
+
+    }
+
+    ngOnInit(): void {
+        this.search()
+    }
+
+
 }
+
+
+
+
